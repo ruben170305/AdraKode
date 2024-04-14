0377960083f6ae890d14885e9ff98e1a3981f088
Adri Arcones <adrianarconesgomez@gmail.com>
2024-04-14 18:31:42 +0200

    Ventana login terminada

--- Programacion/AdraKode/src/views/VentanaPrincipalLogin.java
+++ Programacion/AdraKode/src/views/VentanaPrincipalLogin.java
@@ -8,20 +8,27 @@ import javax.swing.UIManager;
 import javax.swing.WindowConstants;
 import java.awt.Font;
 import java.awt.Color;
+import java.awt.Dimension;
+
 import javax.swing.ImageIcon;
 import java.awt.SystemColor;
 
 import javax.swing.ImageIcon;
 import java.awt.SystemColor;
+
+import javax.swing.border.*;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
 public class VentanaPrincipalLogin extends JPanel {
 
-	private JLabel inicioSesion, usuarioIcono;
-	private JPasswordField contraseña ;
-	private JButton entrar;
-	private JCheckBox administrador;
+	private JLabel inicioSesion, usuarioIcono, contrasenaIcono, logoIcono;
 	private JTextField usuario;
-	
+	private JPasswordField contraseña ;
+	private JCheckBox administrador;
+	private JButton entrar;
+
+
 	//private VentanaPrincipalLogin listener;
 	public VentanaPrincipalLogin (String login) {
 		super ();
@@ -58,40 +65,52 @@ public class VentanaPrincipalLogin extends JPanel {
 		inicioSesion.setForeground(new Color(29, 29, 27));
 		inicioSesion.setHorizontalAlignment(SwingConstants.CENTER);
 		inicioSesion.setFont(new Font("Open Sans", Font.BOLD, 19));
-		inicioSesion.setBounds(215,10,186,45);
+		inicioSesion.setBounds(215,22,186,45);
 		add(inicioSesion);
-		
-		
+
+		//Logo
+	    logoIcono = new JLabel();
+		logoIcono.setIcon(new ImageIcon(VentanaPrincipalLogin.class.getResource("/img/LogoLogin.png")));
+		logoIcono.setBounds(260,62,88,105);
+		add(logoIcono);
 		
 		//Campo usuario
 		usuario = new JTextField();
+		usuario.setBackground(new Color(242, 242, 242));
 		usuario.setFont(new Font("Open Sans", Font.PLAIN, 11));
 		usuario.setForeground(new Color(29, 29, 27));
-	    usuario.setBounds(215, 133, 186, 35); 
+	    usuario.setBounds(215, 163, 186, 35); 
+		Border border = BorderFactory.createMatteBorder(0, 0, 2, 0, new Color(29, 29, 27));
+	    usuario.setBorder(border);
 	    add(usuario);
 		
-	    
 	    //Icono usuario
-	    usuarioIcono = new JLabel("");
+	    usuarioIcono = new JLabel();
 		usuarioIcono.setIcon(new ImageIcon(VentanaPrincipalLogin.class.getResource("/img/IconoUsuario.png")));
-		usuarioIcono.setBounds(175,123,45,45);
+		usuarioIcono.setBounds(181,163,34,45);
 		add(usuarioIcono);
-	
 		
 	    //Campo contraseña
 		contraseña = new JPasswordField();
 		contraseña.setFont(new Font("Open Sans", Font.PLAIN, 11));
 		contraseña.setForeground(new Color(29, 29, 27));
-		contraseña.setBackground(new Color(255, 255, 255));
+		contraseña.setBackground(new Color(242, 242, 242));
 		contraseña.setEchoChar('\u2022'); // Establece el carácter de ocultación 
-		contraseña.setBounds(215,183,186,35);
+		contraseña.setBounds(215,213,186,35);
+	    contraseña.setBorder(border);
 		add(contraseña);
+		
+	    //Icono contraseña
+	    contrasenaIcono = new JLabel();
+		contrasenaIcono.setIcon(new ImageIcon(VentanaPrincipalLogin.class.getResource("/img/IconoContraseña.png")));
+		contrasenaIcono.setBounds(181,213,34,45);
+		add(contrasenaIcono);
 		
 		//Checkbox admin
 		administrador = new JCheckBox("Administrador");
 		administrador.setForeground(new Color(29, 29, 27));
-		administrador.setFont(new Font("Open Sans", Font.PLAIN, 11));
-		administrador.setBounds(251,237,132,20);
+		administrador.setFont(new Font("Open Sans", Font.BOLD, 11));
+		administrador.setBounds(249,267,132,20);
 		add (administrador);
 		
 		//Botón entrar
@@ -101,7 +120,7 @@ public class VentanaPrincipalLogin extends JPanel {
 		entrar.setBackground(new Color(29, 161, 242));
 		entrar.setOpaque(true);
         entrar.setBorderPainted(false);
-		entrar.setBounds(215,287,186,50);
+		entrar.setBounds(215,317,186,50);
 		add(entrar);
 		
 
