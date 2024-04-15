e742fb05aee44d01827078758e5b1b0b9e271f47
RubenPeñaGonzalez <22337489@live.uem.es>
2024-04-15 10:14:33 +0200

    SQL correccion sprint 2

--- BasesDeDatos/Script.sql
+++ BasesDeDatos/Script.sql
@@ -1,21 +1,23 @@
 DROP TABLE juega;
 DROP TABLE utiliza_personaje;
 DROP TABLE partida;
-DROP TABLE miembro;
 DROP TABLE personaje;
+DROP TABLE miembro;
+CREATE TABLE miembro (
+	cod INT PRIMARY KEY,
+    fullname VARCHAR(50),
+    expediente INT,
+    estudio VARCHAR(50)
+);
 CREATE TABLE personaje (
 	cod INT PRIMARY KEY,
     nombre VARCHAR(50),
     personaje VARCHAR(50),
     raza VARCHAR(50),
     clase VARCHAR(50),
-    expe INT
-);
-CREATE TABLE miembro (
-	cod INT PRIMARY KEY,
-    fullname VARCHAR(50),
-    expediente INT,
-    estudio VARCHAR(50)
+    expe INT,
+	cod_miembro VARCHAR(50),
+	CONSTRAINT FOREIGN KEY (cod_miembro) REFERENCES miembro(cod)
 );
 CREATE TABLE partida (
 	cod INT PRIMARY KEY,
@@ -27,13 +29,6 @@ CREATE TABLE partida (
     enCurso boolean,
     controlador INT,
     CONSTRAINT FOREIGN KEY (controlador) REFERENCES miembro(cod)
-);
-CREATE TABLE utiliza_personaje (
-	id_miembro INT,
-    id_personaje INT,
-    PRIMARY KEY (id_miembro, id_personaje),
-    CONSTRAINT FOREIGN KEY (id_miembro) REFERENCES miembro(cod),
-    CONSTRAINT FOREIGN KEY (id_personaje) REFERENCES personaje(cod)
 );
 CREATE TABLE juega (
 	id_partida INT,
@@ -49,13 +44,6 @@ CREATE TABLE juega (
     CONSTRAINT FOREIGN KEY (id_personaje) REFERENCES personaje(cod)
 );
 
-INSERT INTO personaje (cod, nombre, personaje, raza, clase, expe) VALUES
-(1001, 'Eldon', 'Glimmer', 'Elfo', 'Mago', 5000),
-(1002, 'Mara', 'Nightshade', 'Humano', 'Ladrón', 4500),
-(1003, 'Korin', 'Steelbrow', 'Enano', 'Guerrero', 5200),
-(1004, 'Alya', 'Starfire', 'Elfo', 'Clerigo', 4800),
-(1005, 'Borin', 'Hammerhand', 'Enano', 'Paladin', 5500);
-
 INSERT INTO miembro (cod, fullname, expediente, estudio) VALUES
 (2001, 'John Doe', 12345, 'Ciencias Computacionales'),
 (2002, 'Jane Smith', 12346, 'Ingeniería de Software'),
@@ -63,19 +51,19 @@ INSERT INTO miembro (cod, fullname, expediente, estudio) VALUES
 (2004, 'Bob Brown', 12348, 'Ciencias de la Computación'),
 (2005, 'Carol White', 12349, 'Seguridad Informática');
 
+INSERT INTO personaje (cod, nombre, personaje, raza, clase, expe, cod_miembro) VALUES
+(1001, 'Eldon', 'Glimmer', 'Elfo', 'Mago', 5000, 2001),
+(1002, 'Mara', 'Nightshade', 'Humano', 'Ladrón', 4500, 2002),
+(1003, 'Korin', 'Steelbrow', 'Enano', 'Guerrero', 5200, 2003),
+(1004, 'Alya', 'Starfire', 'Elfo', 'Clerigo', 4800, 2004),
+(1005, 'Borin', 'Hammerhand', 'Enano', 'Paladin', 5500, 2005);
+
 INSERT INTO partida (cod, num_sesion, nombre, duracion, dia_hora, ambientacion, enCurso, controlador) VALUES
 (3001, 10, 'Amanecer de fuego', 180, 'Martes a las 17:00', 'Fantasía medieval', true, 2001),
 (3002, 15, 'Crepúsculo gélido', 150, 'Martes a las 19:00', 'Fantasía gélida', false, 2002),
 (3003, 20, 'La caída de la noche', 210, 'Miercoles a las 16:00', 'Fantasía oscura', true, 2003),
 (3004, 25, 'El alba dorada', 160, 'Jueves a las 21:00', 'Fantasía clásica', false, 2004),
 (3005, 30, 'Sombras persistentes', 190, 'Sábado a las 20:00', 'Fantasía steampunk', true, 2005);
-
-INSERT INTO utiliza_personaje (id_miembro, id_personaje) VALUES
-(2001, 1001),
-(2002, 1002),
-(2003, 1003),
-(2004, 1004),
-(2005, 1005);
 
 INSERT INTO juega (id_partida, id_personaje, fuerza, destreza, constitucion, inteligencia, sabiduria, carisma) VALUES
 (3001, 1001, 12, 14, 13, 18, 15, 10),
