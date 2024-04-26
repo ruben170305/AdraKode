f710ee648d5e2f7b80cae39d8bd3d40ba0cebc8f
Adri Arcones <adrianarconesgomez@gmail.com>
2024-04-26 23:07:49 +0200

    Cambios en vista Login. Vista terminada pendiente de revisión

--- Programacion/AdraKode/src/views/VentanaPrincipalLogin.java
+++ Programacion/AdraKode/src/views/VentanaPrincipalLogin.java
@@ -1,19 +1,11 @@
 package views;
 
 import javax.swing.*;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.UIManager;
-import javax.swing.WindowConstants;
+
 import java.awt.Font;
 import java.awt.Color;
 import java.awt.Dimension;
-
-import javax.swing.ImageIcon;
 import java.awt.SystemColor;
-
-import javax.swing.ImageIcon;
 import java.awt.SystemColor;
 
 import javax.swing.border.*;
@@ -22,7 +14,7 @@ import java.awt.event.FocusEvent;
 
 public class VentanaPrincipalLogin extends JPanel {
 
-	private JLabel inicioSesion, usuarioIcono, contrasenaIcono, logoIcono;
+	private JLabel lblTitulo, usuarioIcono, contrasenaIcono, logoIcono;
 	private JTextField usuario;
 	private JPasswordField contraseña ;
 	private JCheckBox administrador;
@@ -59,19 +51,28 @@ public class VentanaPrincipalLogin extends JPanel {
 		//Creo border para cajas de texto (solo linea inferior)
 		Border border = BorderFactory.createMatteBorder(0, 0, 2, 0, new Color(29, 29, 27));
 		
-		//Titulo
-		inicioSesion = new JLabel("INICIAR SESIÓN");
-		inicioSesion.setForeground(new Color(29, 29, 27));
-		inicioSesion.setHorizontalAlignment(SwingConstants.CENTER);
-		inicioSesion.setFont(new Font("Open Sans", Font.BOLD, 25));
-		inicioSesion.setBounds(295,31,209,45);
-		inicioSesion.setBorder(new MatteBorder(0, 0, 3, 0, (Color) new Color(29, 29, 27)));
-		add(inicioSesion);
+		// Titulo
+        lblTitulo = new JLabel("INICIAR SESIÓN");
+        lblTitulo.setForeground(new Color(29, 29, 27));
+        lblTitulo.setHorizontalAlignment(SwingConstants.CENTER);
+        lblTitulo.setFont(new Font("Open Sans", Font.BOLD, 25));
+        lblTitulo.setBounds(269, 32, 261, 45);
+        add(lblTitulo);
+
+        // Separador
+        JSeparator separator = new JSeparator();
+        separator.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+        separator.setPreferredSize(new Dimension(0, 3));
+        separator.setMaximumSize(new Dimension(32778, 32767));
+        separator.setBackground(new Color(29, 29, 27));
+        separator.setForeground(new Color(29, 29, 27));
+        separator.setBounds(337, 88, 125, 3);
+        add(separator);
 
 		//Logo
 	    logoIcono = new JLabel();
 		logoIcono.setIcon(new ImageIcon(VentanaPrincipalLogin.class.getResource("/img/imagenLogin.png")));
-		logoIcono.setBounds(338,124,124,142);
+		logoIcono.setBounds(338,131,124,142);
 		add(logoIcono);
 		
 		//Campo usuario
@@ -79,14 +80,15 @@ public class VentanaPrincipalLogin extends JPanel {
 		usuario.setBackground(new Color(242, 242, 242));
 		usuario.setFont(new Font("Open Sans", Font.PLAIN, 11));
 		usuario.setForeground(new Color(29, 29, 27));
-	    usuario.setBounds(288, 264, 240, 35); 
+	    usuario.setBounds(314, 271, 200, 35); 
 	    usuario.setBorder(border);
 	    add(usuario);
 		
 	    //Icono usuario
 	    usuarioIcono = new JLabel();
+	    usuarioIcono.setHorizontalAlignment(SwingConstants.CENTER);
 		usuarioIcono.setIcon(new ImageIcon(VentanaPrincipalLogin.class.getResource("/img/IconoUsuario.png")));
-		usuarioIcono.setBounds(254,264,34,45);
+		usuarioIcono.setBounds(270,271,34,45);
 		add(usuarioIcono);
 		
 	    //Campo contraseña
@@ -96,21 +98,22 @@ public class VentanaPrincipalLogin extends JPanel {
 		contraseña.setForeground(new Color(29, 29, 27));
 		contraseña.setBackground(new Color(242, 242, 242));
 		contraseña.setEchoChar('\u2022'); // Establece el carácter de ocultación 
-		contraseña.setBounds(288,320,240,35);
+		contraseña.setBounds(314,327,200,35);
 	    contraseña.setBorder(border);
 		add(contraseña);
 		
 	    //Icono contraseña
 	    contrasenaIcono = new JLabel();
+	    contrasenaIcono.setHorizontalAlignment(SwingConstants.CENTER);
 		contrasenaIcono.setIcon(new ImageIcon(VentanaPrincipalLogin.class.getResource("/img/IconoContraseña.png")));
-		contrasenaIcono.setBounds(254,320,34,45);
+		contrasenaIcono.setBounds(268,327,34,45);
 		add(contrasenaIcono);
 		
 		//Checkbox admin
 		administrador = new JCheckBox("Game Master");
 		administrador.setForeground(new Color(29, 29, 27));
 		administrador.setFont(new Font("Open Sans", Font.BOLD, 11));
-		administrador.setBounds(351,384,97,20);
+		administrador.setBounds(351,391,97,20);
 		add (administrador);
 		
 		//Botón entrar
@@ -120,7 +123,7 @@ public class VentanaPrincipalLogin extends JPanel {
 		entrar.setBackground(new Color(29, 161, 242));
 		entrar.setOpaque(true);
         entrar.setBorderPainted(false);
-		entrar.setBounds(295,455,209,50);
+		entrar.setBounds(295,475,209,50);
 		add(entrar);
 		
 
