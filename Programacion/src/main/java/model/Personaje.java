d8ebe0dab74e1d555505b050362b1f5d7712b344
RubenPeñaGonzalez <22337489@live.uem.es>
2024-05-17 13:35:03 +0200

    Insertados metodos en Personaje.java

--- Programacion/src/main/java/model/Personaje.java
+++ Programacion/src/main/java/model/Personaje.java
@@ -82,6 +82,71 @@ public class Personaje extends Data {
 		} 
 		return numero;
 	}
+	
+	public ResultSet conseguir_personajes() {
+
+		ResultSet rs = null;
+
+		// Creamos una conexión con MySQL
+		Model mysql = new Model();
+		mysql.get_connection();
+
+		try {
+			String sql = "SELECT p.*, m.nombre as nombre_anfitrion, m.apellidos as apellidos_anfitrion " +
+			"FROM partida p " +
+			"LEFT JOIN miembro m " +
+			"ON p.anfitrion_id = m.cod";
+
+			rs = mysql.Model_query( sql );
+			return rs;
+		} catch ( SQLException e ) {
+			e.printStackTrace();
+		}
+
+		return rs;
+	}
+	
+	public void update_data() {
+		Model mysql = new Model();
+		mysql.get_connection();
+
+		String update = "UPDATE personaje SET nombre = ?, personaje = ?, raza = ?, clase = ?, expe = ? WHERE cod_miembro = ? AND id_personaje = ?";
+		try (Connection conn = mysql.get_connection(); PreparedStatement pstmt = conn.prepareStatement(update)) {
+			pstmt.setString(1, cPersonaje.getLblSeleccionarPersonaje().getText()); // Suponiendo que 'Personaje' es un
+																					// valor fijo
+			pstmt.setString(2, cPersonaje.getLblSeleccionarPersonaje().getText()); // Suponiendo que 'Personaje' es un
+																					// valor fijo
+			pstmt.setString(3, cPersonaje.getTxtRaza().getText());
+			pstmt.setString(4, cPersonaje.getTxtClase().getText());
+			pstmt.setInt(5, 0); // Si 'expe' es un entero, debes definir cómo se obtiene el valor
+			pstmt.setInt(6, user.getUser_id());
+			pstmt.setInt(7, personaje.getPers_id());
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void delete_data() {
+		Model mysql = new Model();
+		mysql.get_connection();
+		
+		Connection conn = mysql.get_connection();
+		String delete = "DELETE FROM personaje WHERE cod_miembro = ? AND cod = ?";
+		PreparedStatement pstmt;
+		try {
+			pstmt = conn.prepareStatement(delete);
+			
+			pstmt.setInt(1, user.getUser_id());
+			pstmt.setInt(2, personaje.getPers_id());
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
 	public String getNombre() {
 		return nombre;
 	}
